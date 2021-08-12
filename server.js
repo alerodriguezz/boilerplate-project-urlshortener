4609a7683785d7b18bec2d51ab94c8e85ca94cce
arod <alerodriguez@csumb.edu>
2021-08-12 08:36:25 +0000

    initial middleware commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require("body-parser");
 const cors = require('cors');
 const app = express();
 
@@ -10,6 +11,17 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+//middleware to handle post 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api/shorturl', (req, res, next)=>{
+   console.log('data: ', req.body.url);
+   //res.redirect('/');
+  res.json({ original_url: req.body.url });
+});
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +30,15 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+/*
+// Display json
+app.post('/api/shorturl', (req, res)=> {
+  var urlStr = req.body.url
+  res.json({ original_url: `${urlStr}` });
+});
+*/
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
