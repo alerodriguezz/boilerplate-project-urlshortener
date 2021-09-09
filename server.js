48c7186b99bf75dadeed56532444cd5b01d44217
arod <alerodriguez@csumb.edu>
2021-09-09 00:56:45 +0000

    incorporated filter and mongooDB integration

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const dns= require("dns")
 const cors = require('cors');
+const mongoose = require("mongoose");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose.connect( process.env['DB_URL']
+, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
+/*Assign Mongoose Schema to a variable
+This is not necessary but will make your code easier to read */
+const schema = mongoose.Schema({url: 'string' });
+const Url = mongoose.model('Url',schema);
 
 //middleware to handle post 
-
+app.use(cors());
+app.use('/public', express.static(`${process.cwd()}/public`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -22,14 +27,20 @@ app.post('/api/shorturl', (req, res, next)=>{
     try{
     const urlObject = new URL(original_url);
        dns.lookup(urlObject.hostname, (err, address, family) => {  
-
-        if (err) {
-      res.json({error: "invalid URL"});
+            if (err) {
+      res.status(400).json({error: "invalid url"});
     } else{
           let shortened_url = Math.floor(Math.random()*100000).toString();
             // console.log('data: ', req.body.url);
-          res.json({ original_url: original_url,
-          short_url: shortened_url });
+         // res.json({ original_url: original_url,
+          //short_url: shortened_url });
+          const url = new Url({url : original_url})
+          url.save((err,data) => {
+            res.json({
+              original_url: data.url,
+              short_url: data.id
+            })
+          })
     }
     });
 
@@ -46,6 +57,14 @@ app.post('/api/shorturl', (req, res, next)=>{
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+/*
+// Your first API endpoint
+app.get('/api/shorturl/:shorturl', function(req, res) {
+   //let urlStr= req.params.shorturl
+  //res.redirect(301, req.body.url);
+});
+*/
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
